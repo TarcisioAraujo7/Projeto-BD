b9b6e6e514300faf43ac56b967afeb2b02be8cd1
Tarcisio Almeida <tarcisio.olv@gmail.com>
2023-05-09 17:02:50 -0300

    Update crud_jogo.py

--- CRUDS/crud_jogo.py
+++ CRUDS/crud_jogo.py
@@ -3,9 +3,9 @@ from psycopg2 import Error
 from flask import Flask, request, jsonify
 
 # Configuração do banco de dados
-conn = psycopg2.connect(
+conf = psycopg2.connect(
     host="localhost",
-    database="seu_banco_de_dados",
-    user="seu_usuario",
-    password="sua_senha"
+    database="sistema_steam",
+    user="manager",
+    password="123"
 )
